bc934205e81d26ddac7c954be43773c3dfc13fce
swathi <swathisriniwas143@gmail.com>
2023-01-16 14:47:50 +0530

    first commit

---
+++ cube.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "square of the numbers are: [4, 16, 36, 64, 100]\n",
+      "cube of the numbers : [8, 64, 216, 512, 1000]\n"
+     ]
+    }
+   ],
+   "source": [
+    "list1=[2,4,6,8,10]\n",
+    "square=[i**2 for i in list1]\n",
+    "print('square of the numbers are:',square)\n",
+    "cube=[i**3 for i in list1]\n",
+    "print('cube of the numbers :',cube)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
